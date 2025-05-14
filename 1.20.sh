c01fb950a5a1b13c23d2587c8ce462f7d19369b8
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2025-05-14 08:21:43 -0400

    Add a sh and batch script to copy the function folder to the functions folder
    automation at its finest

---
+++ 1.20.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+cp ./data/dragonai/function ./data/dragonai/functions -u -r 2>/dev/null
+cp ./data/dragonai/tags/function ./data/dragonai/tags/functions -u -r 2>/dev/null
+
+cp ./data/z_configmenu/function ./data/z_configmenu/functions -u -r 2>/dev/null
+cp ./data/z_configmenu/tags/function ./data/z_configmenu/tags/functions -u -r 2>/dev/null
+
+cp ./data/minecraft/function ./data/minecraft/functions -u -r 2>/dev/null
+cp ./data/minecraft/tags/function ./data/minecraft/tags/functions -u -r 2>/dev/null
