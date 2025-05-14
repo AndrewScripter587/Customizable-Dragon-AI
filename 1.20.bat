0612142684d662b1de58833f4d6b2150a16a6ffa
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2025-05-14 08:22:34 -0400

    Forgot .bat was in gitignore

---
+++ 1.20.bat
@@ -0,0 +1,8 @@
+copy .\data\dragonai\function .\data\dragonai\functions 
+copy .\data\dragonai\tags\function .\data\dragonai\tags\functions 
+
+copy .\data\z_configmenu\function .\data\z_configmenu\functions 
+copy .\data\z_configmenu\tags\function .\data\z_configmenu\tags\functions 
+
+copy .\data\minecraft\function .\data\minecraft\functions 
+copy .\data\minecraft\tags\function .\data\minecraft\tags\functions 
